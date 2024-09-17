2e133fbf6dc325c335e0aece547bdf3de48aef0d
Mauro Gestoso <maurogestoso@gmail.com>
2024-09-17 17:43:50 +0200

    Add shadcn/ui config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold">Hola mundo 👋 🌍</h1>;
+  return (
+    <main>
+      <h1 className="text-3xl font-bold">Hola mundo 👋 🌍</h1>
+      <Button onClick={() => alert("Made you click")}>Click me!</Button>
+    </main>
+  );
 }
